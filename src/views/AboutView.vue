0dda75001fff04675c88c4a277c18edcf99d4cf7
simonhuchede <simonhuchede@gmail.com>
2023-01-10 17:07:08 +0100

    add about page

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>À Propos de site</h1>
+  <p>Ce site utilise les images de curiosity.</p>
+</template>
+
+<script>
+export default {
+  name: "AboutView"
+}
+</script>
+
+<style scoped>
+
+</style>
